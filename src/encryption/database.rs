d509a9ee1a31c574ad736836df6ec5afc2673e61
imseya18 <imseya68@gmail.com>
2024-08-20 15:08:31 +0200

    refac db

--- src/encryption/database.rs
+++ src/encryption/database.rs
@@ -5,27 +5,30 @@ pub struct Database {
     pub path: String,
     pub db: Connection,
 }
-  
+
 impl  Database {
 
     pub fn init(path: String) -> Result<Self> {
         let conn = Connection::open(&path)?;
         conn.execute_batch(
             "CREATE TABLE if not exists master_profil (
-                id_profil VARCHAR primary key,
+                id_profil INTEGER PRIMARY KEY AUTOINCREMENT,
+                uid_profil TEXT not null unique,
                 name VARCHAR(15) not null unique,
                 master_password VARCHAR not null);
-      
+
             CREATE TABLE if not exists vault (
-                id_vault VARCHAR primary key,
-                id_profil VARCHAR REFERENCES master_profil (id_profil) not null,
+                id_vault INTEGER PRIMARY KEY AUTOINCREMENT,
+                id_profil INTEGER REFERENCES master_profil (id_profil) not null,
+                uid_vault TEXT not null unique,
                 name VARCHAR(15) not null,
                 created_at TIMESTAMP not null,
                 updated_at TIMESTAMP);
-      
+
             CREATE TABLE if not exists account (
-                id_account VARCHAR primary key,
-                id_vault VARCHAR REFERENCES vault (id_vault) not null,
+                id_account INTEGER PRIMARY KEY AUTOINCREMENT,
+                id_vault INTEGER REFERENCES vault (id_vault) not null,
+                uid_account TEXT not null unique,
                 name VARCHAR(20) not null,
                 label VARCHAR(20),
                 account_name VARCHAR(20) not null,
@@ -38,4 +41,3 @@ impl  Database {
         Ok(Database { path, db: conn })
     }
 }
-  

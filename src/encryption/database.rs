66672af4fb4598d62ce39a797c927ad119e2fb3c
DoKca <contact@dokca.com>
2024-08-20 16:25:50 +0200

    encrypt db

--- src/encryption/database.rs
+++ src/encryption/database.rs
@@ -1,4 +1,15 @@
 use rusqlite::{Connection, Result};
+use std::fs::{self, File};
+use std::error::Error;
+use std::path::{PathBuf, Path};
+
+use aes::Aes256;
+use block_modes::{BlockMode, Cbc};
+use block_modes::block_padding::Pkcs7;
+use rand::{Rng, rngs::OsRng};
+use std::io::{self, Write, Read};
+
+type Aes256Cbc = Cbc<Aes256, Pkcs7>;
 
 #[derive(Debug)]
 pub struct Database {
@@ -8,6 +19,31 @@ pub struct Database {
   
 impl  Database {
 
+    /*
+        Take database path and create folder of db path if not exist
+    */
+    pub fn path(path: &str) -> Result<String, Box<dyn Error>> {
+        let db_path = PathBuf::from(path);
+
+        if let Some(dir_path) = db_path.parent()
+        {
+            if !dir_path.exists()
+            {
+                fs::create_dir_all(dir_path)?;
+            }
+                
+        }
+        
+        let db_path_str = db_path.to_str().ok_or_else(|| {
+                rusqlite::Error::InvalidPath(db_path.clone())
+        })?;
+        Ok(db_path_str.to_string())
+    }
+
+
+    /*
+        Connection with sqlite db
+    */
     pub fn init(path: String) -> Result<Self> {
         let conn = Connection::open(&path)?;
         conn.execute_batch(
@@ -36,6 +72,48 @@ impl  Database {
                 updated_at TIMESTAMP)"
         )?;
         Ok(Database { path, db: conn })
+    }
+
+
+    fn is_encrypted(&self) -> io::Result<bool> {
+        let path = Path::new(&self.path);
+        let mut file = File::open(path)?;
+        let mut buffer = [0u8; 16];
+        file.read_exact(&mut buffer)?;
+    
+        Ok(buffer != *b"SQLite format 3\0")
+    }
+
+
+    pub fn encrypt(&self, key: &[u8]) -> Result<bool, Box<dyn Error>> {
+        if self.is_encrypted()?
+        {
+            return Ok(false);
+        }
+
+        let input_path = Path::new(&self.path);
+        let output_path = Path::new(&self.path);
+
+        let mut iv = [0u8; 16]; // 128 bits pour AES
+        OsRng.fill(&mut iv);
+    
+        let data = fs::read(input_path)?;
+    
+        let cipher = Aes256Cbc::new_from_slices(key, &iv).expect("Error creating cipher");
+    
+        let encrypted_data = cipher.encrypt_vec(&data);
+    
+        let mut output_file = File::create(output_path)?;
+    
+        output_file.write_all(&iv)?;
+        output_file.write_all(&encrypted_data)?;
+    
+        Ok(true)
+    }
+
+    pub fn decrypt()
+    {
+
     }
 }
   
--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,28 @@ mod tests;
 mod entities;
 mod encryption;
 
+use std::error::Error;
 use rusqlite::{Result};
+use std::path::{PathBuf, Path};
+
 use crate::entities::*;
 use crate::encryption::*;
 
-fn main() -> Result<()> {
-  //let db = init_database()?;
-  let database = Database::init("rt\\new.db".to_string())?;
-  let new_profil = MasterProfil::new(
-      "35".to_string(),
-      "sdihfoisdhgoishdogisdhgosidhgosdhohsdhgohdh".to_string(),
-      "c'est pas hash".to_string(),
-  );
-  println!("{:?}", database);
-  new_profil.insert(&database.db)?;
-  Ok(())
+fn main() -> Result<(), Box<dyn Error>> {
+	
+	let db_path = Database::path(r"stored\data.db")?;
+    let database = Database::init(db_path)?;
+
+    let new_profil = MasterProfil::new(
+            "35".to_string(),
+            "sdihfoisdhgoishdogisdhgosidhgosdhohsdhgohdh".to_string(),
+            "c'est pas hash".to_string(),
+    );
+    println!("{:?}", database);
+    //new_profil.insert(&database.db)?;
+
+    let key = [0u8; 32];
+	println!("{}", database.encrypt(&key)?);
+
+    Ok(())
 }

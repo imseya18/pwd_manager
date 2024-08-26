61632409890c3172fd67c24e975cbf6227b9cfba
imseya18 <imseya68@gmail.com>
2024-08-26 17:13:15 +0200

    (Account) add sensitiveData struct

--- src/entities/account.rs
+++ src/entities/account.rs
@@ -7,14 +7,18 @@ pub struct Account {
   db_id: Option<i64>,
   vault_id: i64,
   uid: String,
+  sensitive_data: SensitiveData,
+  created_at: i64,
+  updated_at: i64
+}
+
+struct SensitiveData {
   label: Option<String>,
   name: String,
   account_name: String,
   password: String,
   url: Option<String>,
   note: Option<String>,
-  created_at: i64,
-  updated_at: i64
 }
 
 impl Account {
@@ -32,24 +36,26 @@ impl Account {
             db_id: None,
             vault_id,
             uid,
-            label,
-            name,
-            account_name,
-            password,
-            url,
-            note,
+            sensitive_data: SensitiveData{
+                label,
+                name,
+                account_name,
+                password,
+                url,
+                note
+            },
             created_at: Local::now().timestamp(),
             updated_at: Local::now().timestamp(),
         }
     }
 }
 
-impl Insertable for Account {
-  fn insert(&self, db: &Connection) -> Result<()> {
-    db.execute("INSERT INTO account (id_vault, uid_account, name, label, account_name, password, url, note, created_at, updated_at)
-                    VALUES  (?1, ?2, ?3, ?4, ?5, ?6, ?7, ?8, ?9, ?10)",
-      ( &self.vault_id ,&self.uid, &self.name,  &self.label, &self.account_name, &self.password, &self.url, &self.note, &self.created_at, &self.updated_at))
-              .map_err(|e| Box::new(e) as Box<dyn std::error::Error>)?;
-    Ok(())
-  }
-}
+// impl Insertable for Account {
+//   fn insert(&self, db: &Connection) -> Result<()> {
+//     db.execute("INSERT INTO account (id_vault, uid_account, name, label, account_name, password, url, note, created_at, updated_at)
+//                     VALUES  (?1, ?2, ?3, ?4, ?5, ?6, ?7, ?8, ?9, ?10)",
+//       ( &self.vault_id ,&self.uid, &self.name,  &self.label, &self.account_name, &self.password, &self.url, &self.note, &self.created_at, &self.updated_at))
+//               .map_err(|e| Box::new(e) as Box<dyn std::error::Error>)?;
+//     Ok(())
+//   }
+// }
